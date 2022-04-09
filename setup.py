20b2b8da39ac3ec6a785860c6f88c7d62ff63b11
Matthew John <matthew@dockstudios.co.uk>
2022-04-09 19:18:03 +0100

    Issue #26 Add requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session='hack')
 
 setup(name='terrareg',
       version='0.1.0',
@@ -9,4 +12,5 @@ setup(name='terrareg',
       author_email='matthew@dockstudios.co.uk',
       url='https://gitlab.dockstudios.co.uk/pub/terrareg',
       packages=['terrareg'],
+      install_requires=reqs
 )
