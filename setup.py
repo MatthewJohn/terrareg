4fc6679c6fcb0c06d7364c2cd34c831a4ffc4882
Matthew John <matthew@dockstudios.co.uk>
2022-04-09 19:07:36 +0100

    Issue #26 Add example setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='terrareg',
+      version='0.1.0',
+      description='Terraform module regsitry with analytics',
+      author='Matt Comben',
+      author_email='matthew@dockstudios.co.uk',
+      url='https://gitlab.dockstudios.co.uk/pub/terrareg',
+      packages=['terrareg'],
+)
