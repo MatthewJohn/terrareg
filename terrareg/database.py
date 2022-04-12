ea7fc064af9fbf8b83a866ff989621efcfe0eff9
Matthew John <matthew@dockstudios.co.uk>
2022-04-12 06:28:18 +0100

    Issue #18 Add 'verified' columnn to module_version and extract value from terrareg metadata

--- terrareg/database.py
+++ terrareg/database.py
@@ -79,7 +79,8 @@ class Database():
             sqlalchemy.Column('published_at', sqlalchemy.DateTime),
             sqlalchemy.Column('readme_content', sqlalchemy.String),
             sqlalchemy.Column('module_details', sqlalchemy.String),
-            sqlalchemy.Column('variable_template', sqlalchemy.String)
+            sqlalchemy.Column('variable_template', sqlalchemy.String),
+            sqlalchemy.Column('verified', sqlalchemy.Boolean)
         )
 
         self._sub_module = sqlalchemy.Table(
