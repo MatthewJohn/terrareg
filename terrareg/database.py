1ad891e2746d8d75a6fc830945a88a9fd4ead02d
Matthew John <matthew@dockstudios.co.uk>
2022-04-14 07:20:05 +0100

    Issue #56 Update common joined select to return columns from joined module_provider table.
    Switch to use sqlalchemy.select, rather than table.select, as this returns all columns from both tables.

--- terrareg/database.py
+++ terrareg/database.py
@@ -143,6 +143,6 @@ class Database():
 
     def select_module_version_joined_module_provider(self, *args, **kwargs):
         """Perform select on module_version, joined to module_provider table."""
-        return self.module_version.select(*args, **kwargs).join(
+        return sqlalchemy.select(self.module_version.join(
             self.module_provider, self.module_version.c.module_provider_id == self.module_provider.c.id
-        )
+        ))
