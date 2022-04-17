24d6566a686383ca646715b9a09bd931c025b5f8
Matthew John <matthew@dockstudios.co.uk>
2022-04-17 18:55:07 +0100

    Issue #27 Add tests for limit/offset of api module list endpoint

--- test/unit/terrareg/test_server.py
+++ test/unit/terrareg/test_server.py
@@ -7,8 +7,11 @@ from test.unit.terrareg import client
 def test_api_module_list(client):
 
     from terrareg.module_search import ModuleSearch
+
+    # Setup for no modules returned
     ModuleSearch.search_module_providers = MagicMock(return_value=[])
 
+    ## Call with no parameters
     res = client.get('/v1/modules')
 
     assert res.status_code == 200
@@ -16,4 +19,25 @@ def test_api_module_list(client):
         'meta': {'current_offset': 0, 'limit': 10, 'next_offset': 10, 'prev_offset': 0}, 'modules': []
     }
 
+    ModuleSearch.search_module_providers.assert_called_with(provider=None, verified=False, offset=0, limit=10)
+
+    ## Call with limit and offset
+    res = client.get('/v1/modules?offset=23&limit=12')
+
+    assert res.status_code == 200
+    assert res.json == {
+        'meta': {'current_offset': 23, 'limit': 12, 'next_offset': 35, 'prev_offset': 11}, 'modules': []
+    }
+
+    ModuleSearch.search_module_providers.assert_called_with(provider=None, verified=False, offset=23, limit=12)
+
+    ## Call with limit higher than max
+    res = client.get('/v1/modules?offset=65&limit=55')
+
+    assert res.status_code == 200
+    assert res.json == {
+        'meta': {'current_offset': 65, 'limit': 50, 'next_offset': 115, 'prev_offset': 15}, 'modules': []
+    }
+
+    ModuleSearch.search_module_providers.assert_called_with(provider=None, verified=False, offset=65, limit=50)
 
