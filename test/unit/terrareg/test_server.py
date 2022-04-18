d04f4cfd42307f9f0891d0ce1eea468e3491b976
Matthew John <matthew@dockstudios.co.uk>
2022-04-18 10:17:18 +0100

    Issue #27 Add ApiModuleDetails with token extraction

--- test/unit/terrareg/test_server.py
+++ test/unit/terrareg/test_server.py
@@ -334,10 +334,26 @@ class TestApiModuleDetails:
         assert res.status_code == 200
 
     def test_non_existent_module(self, client, mocked_server_module_fixture):
-        """Test endpoint with existing module"""
+        """Test endpoint with non-existent module"""
 
         res = client.get('/v1/modules/doesnotexist/unittestdoesnotexist')
 
         assert res.json == {'errors': ['Not Found']}
         assert res.status_code == 404
 
+
+    def test_analytics_token(self, client, mocked_server_module_fixture):
+        """Test endpoint with analytics token"""
+
+        res = client.get('/v1/modules/test_token-name__testnamespace/testmodulename')
+
+        assert res.json == {
+            'meta': {'limit': 5, 'offset': 0}, 'modules': [
+                {'id': 'testnamespace/testmodulename/testprovider/1.0.0', 'owner': 'Mock Owner',
+                'namespace': 'testnamespace', 'name': 'testmodulename', 'version': '1.0.0',
+                'provider': 'testprovider', 'description': 'Mock description',
+                'source': 'http://mock.example.com/mockmodule',
+                'published_at': '2020-01-01T23:18:12', 'downloads': 0, 'verified': True}
+            ]
+        }
+        assert res.status_code == 200
