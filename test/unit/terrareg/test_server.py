d96db422c43653ff0b91a0b19ecfbf2f051685b6
Matthew John <matthew@dockstudios.co.uk>
2022-04-18 08:21:01 +0100

    Issue #27 Add tests for ApiModuleSearch

--- test/unit/terrareg/test_server.py
+++ test/unit/terrareg/test_server.py
@@ -3,10 +3,11 @@ import sys
 from unittest.mock import MagicMock
 
 from terrareg.models import Namespace, Module
+from terrareg.filters import NamespaceTrustFilter
 from test.unit.terrareg import MockModuleProvider, client
 
 
-def test_api_module_list_default_values(client):
+def test_api_module_list(client):
 
     from terrareg.module_search import ModuleSearch
 
@@ -111,3 +112,160 @@ def test_api_module_list_default_values(client):
 
     ModuleSearch.search_module_providers = unmocked_search_module_providers
 
+
+def test_api_module_search(client):
+    """Test ApiModuleSearch"""
+
+    from terrareg.module_search import ModuleSearch
+
+    # Setup for no modules returned
+    unmocked_search_module_providers = ModuleSearch.search_module_providers
+    ModuleSearch.search_module_providers = MagicMock(return_value=[])
+
+    ## Call with no parameters
+    res = client.get('/v1/modules/search')
+    assert res.status_code == 400
+    ModuleSearch.search_module_providers.assert_not_called()
+
+    ## Call with query param
+    res = client.get('/v1/modules/search?q=unittestteststring')
+
+    assert res.status_code == 200
+    assert res.json == {
+        'meta': {'current_offset': 0, 'limit': 10, 'next_offset': 10, 'prev_offset': 0}, 'modules': []
+    }
+    ModuleSearch.search_module_providers.assert_called_with(
+        query='unittestteststring', namespace=None, provider=None, verified=False,
+        namespace_trust_filters=NamespaceTrustFilter.UNSPECIFIED,
+        offset=0, limit=10)
+
+    ## Call with limit and offset
+    res = client.get('/v1/modules/search?q=test&offset=23&limit=12')
+
+    assert res.status_code == 200
+    assert res.json == {
+        'meta': {'current_offset': 23, 'limit': 12, 'next_offset': 35, 'prev_offset': 11}, 'modules': []
+    }
+    ModuleSearch.search_module_providers.assert_called_with(
+        query='test', namespace=None, provider=None, verified=False,
+        namespace_trust_filters=NamespaceTrustFilter.UNSPECIFIED,
+        offset=23, limit=12)
+
+    ## Call with limit higher than max
+    res = client.get('/v1/modules/search?q=test&offset=65&limit=55')
+
+    assert res.status_code == 200
+    assert res.json == {
+        'meta': {'current_offset': 65, 'limit': 50, 'next_offset': 115, 'prev_offset': 15}, 'modules': []
+    }
+    ModuleSearch.search_module_providers.assert_called_with(
+        query='test', namespace=None, provider=None, verified=False,
+        namespace_trust_filters=NamespaceTrustFilter.UNSPECIFIED,
+        offset=65, limit=50)
+
+    ## Call with provider filter
+    res = client.get('/v1/modules/search?q=test&provider=testprovider')
+
+    assert res.status_code == 200
+    assert res.json == {
+        'meta': {'current_offset': 0, 'limit': 10, 'next_offset': 10, 'prev_offset': 0}, 'modules': []
+    }
+    ModuleSearch.search_module_providers.assert_called_with(
+        query='test', namespace=None, provider='testprovider', verified=False,
+        namespace_trust_filters=NamespaceTrustFilter.UNSPECIFIED,
+        offset=0, limit=10)
+
+    ## Call with namespace filter
+    res = client.get('/v1/modules/search?q=test&namespace=testnamespace')
+
+    assert res.status_code == 200
+    assert res.json == {
+        'meta': {'current_offset': 0, 'limit': 10, 'next_offset': 10, 'prev_offset': 0}, 'modules': []
+    }
+    ModuleSearch.search_module_providers.assert_called_with(
+        query='test', namespace='testnamespace', provider=None, verified=False,
+        namespace_trust_filters=NamespaceTrustFilter.UNSPECIFIED,
+        offset=0, limit=10)
+
+    ## Call with trusted namespace/contributed filters
+    for namespace_filter in [['&trusted_namespaces=false', []],
+                             ['&trusted_namespaces=true', [NamespaceTrustFilter.TRUSTED_NAMESPACES]],
+                             ['&contributed=false', []],
+                             ['&contributed=true', [NamespaceTrustFilter.CONTRIBUTED]],
+                             ['&trusted_namespaces=false&contributed=false', []],
+                             ['&trusted_namespaces=true&contributed=false', [NamespaceTrustFilter.TRUSTED_NAMESPACES]],
+                             ['&trusted_namespaces=false&contributed=true', [NamespaceTrustFilter.CONTRIBUTED]],
+                             ['&trusted_namespaces=true&contributed=true', [NamespaceTrustFilter.TRUSTED_NAMESPACES, NamespaceTrustFilter.CONTRIBUTED]]]:
+
+        res = client.get('/v1/modules/search?q=test{0}'.format(namespace_filter[0]))
+
+        assert res.status_code == 200
+        assert res.json == {
+            'meta': {'current_offset': 0, 'limit': 10, 'next_offset': 10, 'prev_offset': 0}, 'modules': []
+        }
+        ModuleSearch.search_module_providers.assert_called_with(
+            query='test', namespace=None, provider=None, verified=False,
+            namespace_trust_filters=namespace_filter[1],
+            offset=0, limit=10)
+
+    ## Call with verified flag
+    res = client.get('/v1/modules/search?q=test&verified=false')
+
+    assert res.status_code == 200
+    assert res.json == {
+        'meta': {'current_offset': 0, 'limit': 10, 'next_offset': 10, 'prev_offset': 0}, 'modules': []
+    }
+    ModuleSearch.search_module_providers.assert_called_with(
+        query='test', namespace=None, provider=None, verified=False,
+        namespace_trust_filters=NamespaceTrustFilter.UNSPECIFIED,
+        offset=0, limit=10)
+
+    res = client.get('/v1/modules/search?q=test&verified=true')
+
+    assert res.status_code == 200
+    assert res.json == {
+        'meta': {'current_offset': 0, 'limit': 10, 'next_offset': 10, 'prev_offset': 0}, 'modules': []
+    }
+    ModuleSearch.search_module_providers.assert_called_with(
+        query='test', namespace=None, provider=None, verified=True,
+        namespace_trust_filters=NamespaceTrustFilter.UNSPECIFIED,
+        offset=0, limit=10)
+
+    ## Test return of single module module
+    namespace = Namespace(name='testnamespace')
+    module = Module(namespace=namespace, name='mock-module')
+    mock_module_provider = MockModuleProvider(module=module, name='testprovider')
+    mock_module_provider.MOCK_LATEST_VERSION_NUMBER = '1.2.3'
+    ModuleSearch.search_module_providers.return_value = [mock_module_provider]
+
+    res = client.get('/v1/modules/search?q=test&offset=0&limit=1')
+
+    assert res.status_code == 200
+    assert res.json == {
+        'meta': {'current_offset': 0, 'limit': 1, 'next_offset': 1, 'prev_offset': 0}, 'modules': [
+            {'id': 'testnamespace/mock-module/testprovider/1.2.3', 'owner': 'Mock Owner',
+             'namespace': 'testnamespace', 'name': 'mock-module',
+             'version': '1.2.3', 'provider': 'testprovider',
+             'description': 'Mock description', 'source': 'http://mock.example.com/mockmodule',
+             'published_at': '2020-01-01T23:18:12', 'downloads': 0, 'verified': True}
+        ]
+    }
+
+    # Test multiple modules in results
+    mock_namespace_2 = Namespace(name='secondtestnamespace')
+    mock_module_2 = Module(namespace=mock_namespace_2, name='mockmodule2')
+    mock_module_provider_2 = MockModuleProvider(module=mock_module_2, name='secondprovider')
+    mock_module_provider_2.MOCK_LATEST_VERSION_NUMBER = '3.0.0'
+    ModuleSearch.search_module_providers.return_value = [mock_module_provider_2, mock_module_provider]
+
+    res = client.get('/v1/modules/search?q=test&offset=0&limit=2')
+
+    assert res.status_code == 200
+    assert res.json == {
+        'meta': {'current_offset': 0, 'limit': 2, 'next_offset': 2, 'prev_offset': 0}, 'modules': [
+            mock_module_provider_2.get_latest_version().get_api_outline(),
+            mock_module_provider.get_latest_version().get_api_outline(),
+        ]
+    }
+
+    ModuleSearch.search_module_providers = unmocked_search_module_providers
