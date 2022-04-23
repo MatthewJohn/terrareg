af9885de392ea2b45910f07dd634b031aa254a7c
Matthew John <matthew@dockstudios.co.uk>
2022-04-23 07:25:31 +0100

    Issue #63 Add tests for is_authenticated endpoint

--- test/unit/terrareg/test_server.py
+++ test/unit/terrareg/test_server.py
@@ -721,3 +721,20 @@ class TestRequireAdminAuthenticationWrapper:
                 'X-Terrareg-ApiKey': 'testpassword'
             }
         )
+
+class TestApiTerraregIsAuthenticated:
+
+    def test_authenticated(self, client):
+        """Test endpoint when user is authenticated."""
+        with unittest.mock.patch('terrareg.server.check_admin_authentication') as mock_admin_authentication:
+            mock_admin_authentication.return_value = True
+            res = client.get('/v1/terrareg/auth/admin/is_authenticated')
+            assert res.status_code == 200
+            assert res.json == {'authenticated': True}
+
+    def test_unauthenticated(self, client):
+        """Test endpoint when user is authenticated."""
+        with unittest.mock.patch('terrareg.server.check_admin_authentication') as mock_admin_authentication:
+            mock_admin_authentication.return_value = False
+            res = client.get('/v1/terrareg/auth/admin/is_authenticated')
+            assert res.status_code == 401
