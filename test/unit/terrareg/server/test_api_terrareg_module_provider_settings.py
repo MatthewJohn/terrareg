c90e574186d17f9af92c42cca6808a05e6c54dfc
Matt Comben <matt.comben@jobsite.co.uk>
2022-04-28 16:51:32 +0100

    Issue #77 Update ModuleProviderSettings tests for new git provider URL arguments/functions

--- test/unit/terrareg/server/test_api_terrareg_module_provider_settings.py
+++ test/unit/terrareg/server/test_api_terrareg_module_provider_settings.py
@@ -31,11 +31,10 @@ class TestApiTerraregModuleProviderSettings(TerraregUnitTest):
                 unittest.mock.patch('terrareg.models.ModuleProvider.update_git_tag_format') as mock_update_git_tag_format, \
                 unittest.mock.patch('terrareg.models.ModuleProvider.update_attributes') as mock_update_attributes:
 
-            print(repository_url)
             res = client.post(
                 '/v1/terrareg/modules/testnamespace/testmodulename/testprovider/settings',
                 json={
-                    'repository_url': repository_url,
+                    'repo_clone_url_template': repository_url,
                     'csrf_token': 'unittestcsrf'
                 }
             )
@@ -46,7 +45,7 @@ class TestApiTerraregModuleProviderSettings(TerraregUnitTest):
             mock_check_csrf.assert_called_once_with('unittestcsrf')
             mocked_check_admin_authentication.assert_called()
             mock_update_git_tag_format.assert_not_called()
-            mock_update_attributes.assert_called_once_with(repository_url=repository_url)
+            mock_update_attributes.assert_called_once_with(repo_clone_url_template=repository_url)
 
     @setup_test_data()
     def test_update_repository_url_invalid_protocol(self, app_context, test_request_context, mocked_server_namespace_fixture, client):
@@ -60,11 +59,11 @@ class TestApiTerraregModuleProviderSettings(TerraregUnitTest):
             res = client.post(
                 '/v1/terrareg/modules/testnamespace/testmodulename/testprovider/settings',
                 json={
-                    'repository_url': 'nope://unittest.com/module.git',
+                    'repo_clone_url_template': 'nope://unittest.com/module.git',
                     'csrf_token': 'unittestcsrf'
                 }
             )
-            assert res.json == {'message': 'Repository URL contains an unknown scheme (e.g. https/git/http)'}
+            assert res.json == {'message': 'Repository URL contains an unknown scheme (e.g. https/ssh/http)'}
             assert res.status_code == 400
 
             # Ensure required checks are called
@@ -85,7 +84,7 @@ class TestApiTerraregModuleProviderSettings(TerraregUnitTest):
             res = client.post(
                 '/v1/terrareg/modules/testnamespace/testmodulename/testprovider/settings',
                 json={
-                    'repository_url': 'https:///module.git',
+                    'repo_clone_url_template': 'https:///module.git',
                     'csrf_token': 'unittestcsrf'
                 }
             )
@@ -110,7 +109,7 @@ class TestApiTerraregModuleProviderSettings(TerraregUnitTest):
             res = client.post(
                 '/v1/terrareg/modules/testnamespace/testmodulename/testprovider/settings',
                 json={
-                    'repository_url': 'https://example.com',
+                    'repo_clone_url_template': 'https://example.com',
                     'csrf_token': 'unittestcsrf'
                 }
             )
@@ -128,24 +127,22 @@ class TestApiTerraregModuleProviderSettings(TerraregUnitTest):
         """Test update of repository URL without a CSRF token."""
         with app_context, test_request_context, client, \
                 unittest.mock.patch('terrareg.server.check_admin_authentication', return_value=True) as mocked_check_admin_authentication, \
-                unittest.mock.patch('terrareg.server.check_csrf_token', return_value=True) as mock_check_csrf, \
                 unittest.mock.patch('terrareg.models.ModuleProvider.update_git_tag_format') as mock_update_git_tag_format, \
                 unittest.mock.patch('terrareg.models.ModuleProvider.update_attributes') as mock_update_attributes:
 
             res = client.post(
                 '/v1/terrareg/modules/testnamespace/testmodulename/testprovider/settings',
                 json={
-                    'repository_url': 'https://example.com/test.git'
+                    'repo_clone_url_template': 'https://example.com/test.git'
                 }
             )
-            assert res.json == {}
-            assert res.status_code == 200
+            assert res.json == {'message': 'No session is presesnt to check CSRF token', 'status': 'Error'}
+            assert res.status_code == 500
 
             # Ensure required checks are called
-            mock_check_csrf.assert_called_once_with(None)
             mocked_check_admin_authentication.assert_called()
             mock_update_git_tag_format.assert_not_called()
-            mock_update_attributes.assert_called_once_with(repository_url='https://example.com/test.git')
+            mock_update_attributes.assert_not_called()
 
     @setup_test_data()
     def test_update_git_tag_format(
