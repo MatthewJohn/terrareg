aa3f788e117cc7ba88d0a71493803139ab532847
Matthew John <matthew@dockstudios.co.uk>
2022-04-30 08:00:57 +0100

    Issue #89 Add tests to verify that browse URL is generated correctly

--- test/integration/terrareg/models/test_module_provider.py
+++ test/integration/terrareg/models/test_module_provider.py
@@ -2,7 +2,7 @@
 from unittest import mock
 import pytest
 
-from terrareg.models import Module, Namespace, ModuleProvider
+from terrareg.models import Module, ModuleVersion, Namespace, ModuleProvider
 import terrareg.errors
 from test.integration.terrareg import (
     TerraregIntegrationTest,
@@ -80,3 +80,45 @@ class TestModuleProvider(TerraregIntegrationTest):
             '0.1.10', '0.1.09', '0.1.8',
             '0.1.1', '0.0.9'
         ]
+
+    @setup_test_data()
+    @pytest.mark.parametrize('module_name,module_version,path,expected_browse_url', [
+        # Test no browse URL in any configuration
+        ('no-git-provider', '1.0.0', None, None),
+        ('no-git-provider', '1.0.0', 'unittestpath', None),
+        # Test browse URL only configured in module version
+        ('no-git-provider', '1.4.0', None, 'https://mv-browse-url.com/repo_url_tests/no-git-provider-test/browse/1.4.0/suffix'),
+        ('no-git-provider', '1.4.0', 'unittestpath', 'https://mv-browse-url.com/repo_url_tests/no-git-provider-test/browse/1.4.0/unittestpathsuffix'),
+
+        # Test with git provider configured in module provider
+        ('git-provider-urls', '1.1.0', None, 'https://browse-url.com/repo_url_tests/git-provider-urls-test/browse/1.1.0/suffix'),
+        ('git-provider-urls', '1.1.0', 'unittestpath', 'https://browse-url.com/repo_url_tests/git-provider-urls-test/browse/1.1.0/unittestpathsuffix'),
+        # Test with git provider configured in module provider and override in module version
+        ('git-provider-urls', '1.4.0', None, 'https://mv-browse-url.com/repo_url_tests/git-provider-urls-test/browse/1.4.0/suffix'),
+        ('git-provider-urls', '1.4.0', 'unittestpath', 'https://mv-browse-url.com/repo_url_tests/git-provider-urls-test/browse/1.4.0/unittestpathsuffix'),
+
+        # Test with git provider configured in module provider
+        ('module-provider-urls', '1.2.0', None, 'https://mp-browse-url.com/repo_url_tests/module-provider-urls-test/browse/1.2.0/suffix'),
+        ('module-provider-urls', '1.2.0', 'unittestpath', 'https://mp-browse-url.com/repo_url_tests/module-provider-urls-test/browse/1.2.0/unittestpathsuffix'),
+        # Test with URls configured in module provider and override in module version
+        ('module-provider-urls', '1.4.0', None, 'https://mv-browse-url.com/repo_url_tests/module-provider-urls-test/browse/1.4.0/suffix'),
+        ('module-provider-urls', '1.4.0', 'unittestpath', 'https://mv-browse-url.com/repo_url_tests/module-provider-urls-test/browse/1.4.0/unittestpathsuffix'),
+
+        # Test with git provider configured in module provider and URLs configured in git provider
+        ('module-provider-override-git-provider', '1.3.0', None, 'https://mp-browse-url.com/repo_url_tests/module-provider-override-git-provider-test/browse/1.3.0/suffix'),
+        ('module-provider-override-git-provider', '1.3.0', 'unittestpath', 'https://mp-browse-url.com/repo_url_tests/module-provider-override-git-provider-test/browse/1.3.0/unittestpathsuffix'),
+        # Test with URls configured in module provider and override in module version
+        ('module-provider-override-git-provider', '1.4.0', None, 'https://mv-browse-url.com/repo_url_tests/module-provider-override-git-provider-test/browse/1.4.0/suffix'),
+        ('module-provider-override-git-provider', '1.4.0', 'unittestpath', 'https://mv-browse-url.com/repo_url_tests/module-provider-override-git-provider-test/browse/1.4.0/unittestpathsuffix')
+    ])
+    def test_get_source_browse_url(self, module_name, module_version, path, expected_browse_url):
+        """Ensure browse URL matches the expected values."""
+        namespace = Namespace(name='repo_url_tests')
+        module = Module(namespace=namespace, name=module_name)
+        module_provider = ModuleProvider.get(module=module, name='test')
+        assert module_provider is not None
+        module_version = ModuleVersion.get(module_provider=module_provider, version=module_version)
+        assert module_version is not None
+
+        kwargs = {'path': path} if path else {}
+        assert module_version.get_source_browse_url(**kwargs) == expected_browse_url
