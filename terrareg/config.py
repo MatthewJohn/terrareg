75e74363dd18f1a5b41276fe967533ae674300c1
Matthew John <matthew@dockstudios.co.uk>
2022-05-02 07:52:12 +0100

    Issue #83 Create script to generate markdown documentation for Config.
    Update docstrings to make use of markdown code blocks

--- terrareg/config.py
+++ terrareg/config.py
@@ -98,13 +98,13 @@ class Config:
         """
         List of comma-separated values for terraform auth tokens for deployment environments.
 
-        E.g. xxxxxx.deploy1.xxxxxxxxxxxxx:dev,zzzzzz.deploy1.zzzzzzzzzzzzz:prod
-        In this example, in the 'dev' environment, the auth token for terraform would be: xxxxxx.deploy1.xxxxxxxxxxxxx
-        and the auth token for terraform for prod would be: zzzzzz.deploy1.zzzzzzzzzzzzz.
+        E.g. `xxxxxx.deploy1.xxxxxxxxxxxxx:dev,zzzzzz.deploy1.zzzzzzzzzzzzz:prod`
+        In this example, in the 'dev' environment, the auth token for terraform would be: `xxxxxx.deploy1.xxxxxxxxxxxxx`
+        and the auth token for terraform for prod would be: `zzzzzz.deploy1.zzzzzzzzzzzzz`.
 
         To disable auth tokens and to report all downloads, leave empty.
 
-        To only record downloads in a single environment, specify a single auth token. E.g. 'zzzzzz.deploy1.zzzzzzzzzzzzz'
+        To only record downloads in a single environment, specify a single auth token. E.g. `zzzzzz.deploy1.zzzzzzzzzzzzz`
         """
         return [
             token for token in os.environ.get('ANALYTICS_AUTH_KEYS', '').split(',') if token
@@ -120,7 +120,7 @@ class Config:
         """
         Flask secret key used for encrypting sessions.
 
-        Can be generated using: python -c 'import secrets; print(secrets.token_hex())'
+        Can be generated using: `python -c 'import secrets; print(secrets.token_hex())'`
         """
         return os.environ.get('SECRET_KEY', None)
 
@@ -134,8 +134,10 @@ class Config:
         """
         Whether new module versions (either via upload, import or hook) are automatically
         published and available.
+
         If this is disabled, the publish endpoint must be called before the module version
         is displayed in the list of module versions.
+
         NOTE: Even whilst in an unpublished state, the module version can still be accessed directly, but not used within terraform.
         """
         return os.environ.get('AUTO_PUBLISH_MODULE_VERSIONS', 'True') == 'True'
@@ -146,7 +148,7 @@ class Config:
 
         submodules are expected to be within sub-directories of the submodule directory.
 
-        E.g. If MODULES_DIRECTORY is set to 'modules', with the root module, the following would be expected for a submodule: 'modules/submodulename/main.tf'.
+        E.g. If MODULES_DIRECTORY is set to `modules`, with the root module, the following would be expected for a submodule: `modules/submodulename/main.tf`.
 
         This can be set to an empty string, to expected submodules to be in the root directory of the parent module.
         """
@@ -158,7 +160,7 @@ class Config:
 
         Examples are expected to be within sub-directories of the examples directory.
 
-        E.g. If EXAMPLES_DIRECTORY is set to 'examples', with the root module, the following would be expected for an example: 'examples/myexample/main.tf'.
+        E.g. If EXAMPLES_DIRECTORY is set to `examples`, with the root module, the following would be expected for an example: `examples/myexample/main.tf`.
         """
         return os.environ.get('MODULES_DIRECTORY', 'examples')
 
@@ -183,9 +185,11 @@ class Config:
                         or 'https://bitbucket.org/{namespace}/{module}/src/{version}')
 
         An example for public repositories might be:
+        ```
         [{"name": "Github", "base_url": "https://github.com/{namespace}/{module}", "clone_url": "ssh://git@github.com:{namespace}/{module}.git", "browse_url": "https://github.com/{namespace}/{module}/tree/{tag}/{path}"},
         {"name": "Bitbucket", "base_url": "https://bitbucket.org/{namespace}/{module}", "clone_url": "ssh://git@bitbucket.org:{namespace}/{module}-{provider}.git", "browse_url": "https://bitbucket.org/{namespace}/{module}-{provider}/src/{tag}/{path}"},
         {"name": "Gitlab", "base_url": "https://gitlab.com/{namespace}/{module}", "clone_url": "ssh://git@gitlab.com:{namespace}/{module}-{provider}.git", "browse_url": "https://gitlab.com/{namespace}/{module}-{provider}/-/tree/{tag}/{path}"}]
+        ```
         """
         return os.environ.get('GIT_PROVIDER_CONFIG', '[]')
 
