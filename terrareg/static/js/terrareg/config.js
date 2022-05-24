7eb0e2a1a3d5fb8ab217f8a86a80c07a5551d543
Matthew John <matthew@dockstudios.co.uk>
2022-05-24 06:09:15 +0100

    Use singleton promise for getConfig method, which is returned each time. This means that a single call to obtain the config is ever created.
    Issue #133

--- terrareg/static/js/terrareg/config.js
+++ terrareg/static/js/terrareg/config.js
@@ -1,22 +1,21 @@
 
-terraregConfig = undefined;
+terraregConfigPromiseSingleton = undefined;
 
 async function getConfig() {
-    return new Promise((resolve, reject) => {
-
-        // Check if terrareg config has already been obtained
-        if (terraregConfig !== undefined) {
-            resolve(terraregConfig);
-        }
-
-        // If not, perform request
-        $.ajax({
-            type: "GET",
-            url: "/v1/terrareg/config",
-            success: function (data) {
-                terraregConfig = data;
-                resolve(terraregConfig);
-            }
+    // Create promise if it hasn't already been defined
+    if (terraregConfigPromiseSingleton === undefined) {
+        terraregConfigPromiseSingleton = new Promise((resolve, reject) => {
+            // Perform request to obtain the config
+            $.ajax({
+                type: "GET",
+                url: "/v1/terrareg/config",
+                success: function (data) {
+                    terraregConfig = data;
+                    resolve(terraregConfig);
+                }
+            });
         });
-    });
+    }
+    return terraregConfigPromiseSingleton;
 }
+
