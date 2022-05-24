ddeb54963fb2abbb5a9539c5e92bde51423ba31b
Matthew John <matthew@dockstudios.co.uk>
2022-05-24 06:04:14 +0100

    fix: Fix tiles on homepage occasionally not loading when config request has not completed in time.
    The current method caused a race condition where the request for the config expected to have completed by the time the request for most download/most recent modules had completed.
    Create getConfig method that returns a promise for the config, which is awaited whilst creating results card.
    
    Issue #133

--- terrareg/static/js/terrareg/config.js
+++ terrareg/static/js/terrareg/config.js
@@ -1,11 +1,22 @@
 
 terraregConfig = undefined;
-$(document).ready(function () {
-    $.ajax({
-        type: "GET",
-        url: "/v1/terrareg/config",
-        success: function (data) {
-            terraregConfig = data;
+
+async function getConfig() {
+    return new Promise((resolve, reject) => {
+
+        // Check if terrareg config has already been obtained
+        if (terraregConfig !== undefined) {
+            resolve(terraregConfig);
         }
+
+        // If not, perform request
+        $.ajax({
+            type: "GET",
+            url: "/v1/terrareg/config",
+            success: function (data) {
+                terraregConfig = data;
+                resolve(terraregConfig);
+            }
+        });
     });
-});
+}
