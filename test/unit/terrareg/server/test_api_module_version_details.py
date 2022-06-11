5e6698733aa9ca40dbf7dcde830b3b5dbd1a3197
Matthew John <matthew@dockstudios.co.uk>
2022-06-11 22:17:50 +0100

    test: Add unittest test to check internal module version flag is set correctly in api outline
    Issue #137

--- test/unit/terrareg/server/test_api_module_version_details.py
+++ test/unit/terrareg/server/test_api_module_version_details.py
@@ -56,6 +56,28 @@ class TestApiModuleVersionDetails(TerraregUnitTest):
         assert res.status_code == 200
 
     @setup_test_data()
+    def test_internal_module_version(self, client, mocked_server_namespace_fixture):
+        res = client.get('/v1/modules/testnamespace/internalmodule/testprovider/5.2.0')
+
+        assert res.json == {
+            'id': 'testnamespace/internalmodule/testprovider/5.2.0', 'owner': 'Mock Owner',
+            'namespace': 'testnamespace', 'name': 'internalmodule',
+            'version': '5.2.0', 'provider': 'testprovider',
+            'description': 'Mock description',
+            'source': None,
+            'published_at': '2020-01-01T23:18:12',
+            'downloads': 0, 'verified': False, 'trusted': False, 'internal': True,
+            'root': {
+                'path': '', 'readme': 'Mock module README file',
+                'empty': False, 'inputs': [], 'outputs': [], 'dependencies': [],
+                'provider_dependencies': [], 'resources': []
+            },
+            'submodules': [], 'providers': ['testprovider'], 'versions': ['5.2.0']
+        }
+
+        assert res.status_code == 200
+
+    @setup_test_data()
     def test_non_existent_module_version(self, client, mocked_server_namespace_fixture):
         """Test endpoint with non-existent module"""
 
