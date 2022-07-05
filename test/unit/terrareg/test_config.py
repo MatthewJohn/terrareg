dc3d7621436626240367aec13cf735154c91601f
Matthew John <matthew@dockstudios.co.uk>
2022-07-05 17:51:18 +0100

    test: Add unit test to test new boolean config: ENABLE_SECURITY_SCANNING
    Issue #150

--- test/unit/terrareg/test_config.py
+++ test/unit/terrareg/test_config.py
@@ -130,7 +130,8 @@ class TestConfig:
         'DEBUG',
         'DELETE_EXTERNALLY_HOSTED_ARTIFACTS',
         'DISABLE_TERRAREG_EXCLUSIVE_LABELS',
-        'AUTOGENERATE_MODULE_PROVIDER_DESCRIPTION'
+        'AUTOGENERATE_MODULE_PROVIDER_DESCRIPTION',
+        'ENABLE_SECURITY_SCANNING'
     ])
     def test_boolean_configs(self, config_name, test_value, expected_value):
         """Test boolean configs to ensure they are overriden with environment variables."""
