3d26ef10324e1b5fdd97f2a1a4c9748a568cb912
Matthew John <matthew@dockstudios.co.uk>
2022-07-24 13:43:38 +0100

    test: Add tests to ensure safe_join_paths raises error when traversing outside of root directory
    Issue #69

--- test/unit/terrareg/test_utils.py
+++ test/unit/terrareg/test_utils.py
@@ -1,7 +1,7 @@
 
 import pytest
 
-from terrareg.utils import safe_join_paths
+import terrareg.utils
 from test.unit.terrareg import TerraregUnitTest
 
 
@@ -19,7 +19,30 @@ class TestSafeJoinPaths(TerraregUnitTest):
 
         # Leading slash in subdirectory
         ('/root_dir', ['/leadingdotslash'], '/root_dir/leadingdotslash'),
+
+        # Test with relative path within sub-directory
+        ('/root_dir', ['/subdirectory', '../actualdirectory'], '/root_dir/actualdirectory'),
+
     ])
     def test_valid_paths(self, base_dir, sub_paths, expected_output):
         """Test valid path using safe_join_paths method."""
-        assert safe_join_paths(base_dir, *sub_paths) == expected_output
+        assert terrareg.utils.safe_join_paths(base_dir, *sub_paths) == expected_output
+
+    @pytest.mark.parametrize('base_dir,sub_paths', [
+        # Basic test
+        ('/root_dir', ['../subdirectory']),
+
+        # Multiple sub-directories with exit root directory on first subdir
+        ('/root_dir', ['../../subdirectory', 'subdir2']),
+
+        # Leading exit root directory in second sub directory 
+        ('/root_dir', ['./tosubdir', '../../outofrootdir']),
+
+        # Leading slash in subdirectory
+        ('/root_dir', ['/../../leadingdotslash']),
+    ])
+    def test_invalid_paths(self, base_dir, sub_paths):
+        """Test valid path using safe_join_paths method."""
+        with pytest.raises(terrareg.utils.PathIsNotWithinBaseDirectoryError):
+            assert terrareg.utils.safe_join_paths(base_dir, *sub_paths)
+
