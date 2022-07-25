66edbed8117688bed4bff8ac22e9cc53080165a5
Matthew John <matthew@dockstudios.co.uk>
2022-07-25 07:40:56 +0100

    Add missing import of Database class in test_module_provider, caused by rebase of changes to branch
    Issue #93

--- test/integration/terrareg/models/test_module_provider.py
+++ test/integration/terrareg/models/test_module_provider.py
@@ -1,6 +1,7 @@
 
 from unittest import mock
 import pytest
+from terrareg.database import Database
 
 from terrareg.models import Module, ModuleVersion, Namespace, ModuleProvider
 import terrareg.errors
