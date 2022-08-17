a099cb6bee69889d7fa9fcbe24e358f2846f30a2
Matthew John <matthew@dockstudios.co.uk>
2022-08-17 21:21:54 +0100

    test: Use assert_equals in test_search_from_homepage to improve reliability

--- test/selenium/test_module_search.py
+++ test/selenium/test_module_search.py
@@ -33,8 +33,7 @@ class TestModuleSearch(SeleniumTest):
         assert search_button.text == 'Search'
         search_button.click()
 
-        self.assert_equals(lambda: self.selenium_instance.current_url, self.get_url('/modules/search?q=modulesearch'))
-
+        assert self.assert_equals(lambda x: self.selenium_instance.current_url, self.get_url('/modules/search?q=modulesearch'))
         assert self.selenium_instance.title == 'Search - Terrareg'
 
         self.assert_equals(lambda: len(self.selenium_instance.find_element(By.ID, 'results').find_elements(By.CLASS_NAME, 'card')), 4)
