eea4a433399039010f3f05baca77cf2479041ee0
Matthew John <matthew@dockstudios.co.uk>
2022-09-07 07:27:57 +0100

    Draft implementation of flow up to obtaining token.
    Add button to UI to sign in via SSO.
    Add endpoints to initiate authentication flow and handle callback from provider
    
    Issue #7

--- terrareg/openid_connect.py
+++ terrareg/openid_connect.py
@@ -1,31 +1,89 @@
+import random
+import string
 
 import requests
 from oauthlib.oauth2 import WebApplicationClient
 
 import terrareg.config
 
-class OpenidConnection:
+class OpenidConnect:
 
-    def is_enabled(self):
+    _METADATA_CONFIG = None
+
+    @classmethod
+    def is_enabled(cls):
         """Whether OpenID connect authentication is enabled"""
         config = terrareg.config.Config()
-        return bool(config.OPENID_CONNECT_CLIENT_ID and config.OPENID_CONNECT_CLIENT_SECRET and config.OPENID_CONNECT_AUTH_URL and config.DOMAIN_NAME)
+        return bool(config.OPENID_CONNECT_CLIENT_ID and config.OPENID_CONNECT_CLIENT_SECRET and config.OPENID_CONNECT_ISSUER and config.DOMAIN_NAME)
 
     def get_client():
         """Return oauth2 web application client"""
         return WebApplicationClient(terrareg.config.Config().OPENID_CONNECT_CLIENT_ID)
 
-    @classmethod
-    def get_authorize_url(cls):
-        """Get authorize URL to redirect user to for authentication"""
+    @staticmethod
+    def get_redirect_url():
+        """Obtain redirect URL for Terrareg instance"""
         config = terrareg.config.Config()
-        return cls.get_client().prepare_request_uri(
-            config.OPENID_CONNECT_AUTH_URL,
-            redirect_uri = f'https://{config.DOMAIN_NAME}/openid/callback',
-            scope = ['read:user'],
-            state = 'D8VAo311AAl_49LAtM51HA'
-        )
+        return f'https://{config.DOMAIN_NAME}/openid/callback'
 
     @classmethod
-    def fetch_access_token():
-        pass
+    def obtain_issuer_metadata(cls):
+        """Obtain wellknown metadata from issuer"""
+        # Obtain meta data from well-known URL, if not previously cached
+        if not cls.is_enabled():
+            return None
+
+        if cls._METADATA_CONFIG is None:
+            res = requests.get(terrareg.config.Config().OPENID_CONNECT_ISSUER + '/.well-known/openid-configuration')
+            cls._METADATA_CONFIG = res.json()
+
+        return cls._METADATA_CONFIG
+
+    @classmethod
+    def generate_state(cls):
+        """Return random string for state"""
+        letters = string.ascii_letters + string.digits
+        return ''.join(random.choice(letters) for i in range(24))
+
+    @classmethod
+    def get_authorize_redirect_url(cls):
+        """Get authorize URL to redirect user to for authentication"""
+        if not cls.is_enabled:
+            return None, None
+
+        auth_url = cls.obtain_issuer_metadata().get('authorization_endpoint', None)
+        if not auth_url:
+            return None, None
+
+        config = terrareg.config.Config()
+
+        state = cls.generate_state()
+
+        return cls.get_client().prepare_request_uri(
+            auth_url,
+            redirect_uri=cls.get_redirect_url(),
+            scope=['openid', 'profile'],
+            state=state
+        ), state
+
+    @classmethod
+    def fetch_access_token(cls, code):
+        """Fetch access token from OpenID issuer"""
+        client = cls.get_client()
+        config = terrareg.config.Config()
+        token_request_body = client.prepare_request_body(
+            code=code,
+            client_id=config.OPENID_CONNECT_CLIENT_ID,
+            client_secret=config.OPENID_CONNECT_CLIENT_SECRET,
+            redirect_uri=cls.get_redirect_url()
+        )
+
+        token_endpoint = cls.obtain_issuer_metadata().get('token_endpoint', None)
+        if not token_endpoint:
+            return None
+        response = requests.post(token_endpoint, token_request_body)
+        print(response.text)
+
+        return client.parse_request_body_response(response.text)
+
+
