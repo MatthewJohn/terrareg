788859ffea53a6413f8deaf9b44d00a7d74b8fde
Matthew John <matthew@dockstudios.co.uk>
2022-10-16 08:48:23 +0100

    test: Update tests for IsAuthenticated and AdminAuthenticate to mock new auth framework
    Issue #255

--- test/unit/terrareg/server/test_api_terrareg_is_authenticated.py
+++ test/unit/terrareg/server/test_api_terrareg_is_authenticated.py
@@ -9,15 +9,19 @@ class TestApiTerraregIsAuthenticated(TerraregUnitTest):
 
     def test_authenticated(self, client):
         """Test endpoint when user is authenticated."""
-        with unittest.mock.patch('terrareg.server.check_admin_authentication') as mock_admin_authentication:
-            mock_admin_authentication.return_value = True
+        mock_auth_method = unittest.mock.MagicMock()
+        mock_auth_method.is_authenticated = unittest.mock.MagicMock(return_value=True)
+        mock_get_current_auth_method = unittest.mock.MagicMock(return_value=mock_auth_method)
+        with unittest.mock.patch('terrareg.auth.AuthFactory.get_current_auth_method', mock_get_current_auth_method):
             res = client.get('/v1/terrareg/auth/admin/is_authenticated')
             assert res.status_code == 200
             assert res.json == {'authenticated': True}
 
     def test_unauthenticated(self, client):
         """Test endpoint when user is authenticated."""
-        with unittest.mock.patch('terrareg.server.check_admin_authentication') as mock_admin_authentication:
-            mock_admin_authentication.return_value = False
+        mock_auth_method = unittest.mock.MagicMock()
+        mock_auth_method.is_authenticated = unittest.mock.MagicMock(return_value=False)
+        mock_get_current_auth_method = unittest.mock.MagicMock(return_value=mock_auth_method)
+        with unittest.mock.patch('terrareg.auth.AuthFactory.get_current_auth_method', mock_get_current_auth_method):
             res = client.get('/v1/terrareg/auth/admin/is_authenticated')
             assert res.status_code == 401
