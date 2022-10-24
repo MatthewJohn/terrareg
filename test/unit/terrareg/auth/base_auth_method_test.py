5ffc61dc79417d2077ea9fa1408aac7c62f0bdb9
Matthew John <matthew@dockstudios.co.uk>
2022-10-24 07:45:58 +0100

    test: Add tests for NotAuthenticated auth class and base class to define required tests.
    Issue #255

---
+++ test/unit/terrareg/auth/base_auth_method_test.py
@@ -0,0 +1,55 @@
+
+import contextlib
+import unittest.mock
+import datetime
+
+import pytest
+import werkzeug.exceptions
+import jwt
+
+from test.unit.terrareg import TerraregUnitTest
+from terrareg.auth import AuthFactory, AuthenticationType
+
+
+class BaseAuthMethodTest(TerraregUnitTest):
+    """Test common smaller methods of auth methods"""
+
+    def test_is_built_in_admin(self):
+        """Test is_built_in_admin method"""
+        raise NotImplementedError
+
+    def test_is_admin(self):
+        """Test is_admin method"""
+        raise NotImplementedError
+
+    def test_is_authenticated(self):
+        """Test is_authenticated method"""
+        raise NotImplementedError
+
+    def test_is_enabled(self):
+        """Test is_enabled method"""
+        raise NotImplementedError
+
+    def test_requires_csrf_tokens(self):
+        """Test requires_csrf_token method"""
+        raise NotImplementedError
+
+    def test_can_publish_module_version(self):
+        """Test can_publish_module_version method"""
+        raise NotImplementedError
+
+    def test_can_upload_module_version(self):
+        """Test can_upload_module_version method"""
+        raise NotImplementedError
+
+    def test_get_current_instance(self):
+        """Test get_current_intstance method"""
+        raise NotImplementedError
+
+    def test_check_auth_state(self):
+        """Test check_auth_state method"""
+        raise NotImplementedError
+
+    def test_check_namespace_access(self):
+        """Test check_namespace_access method"""
+        raise NotImplementedError
