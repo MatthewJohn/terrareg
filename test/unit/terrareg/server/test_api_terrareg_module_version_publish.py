a195b5058e4b64fc8af8f7fb26e561ffb11641d7
Matthew John <matthew@dockstudios.co.uk>
2022-10-30 07:39:18 +0000

    Fix expected response when not authenticated in ApiModuleVersionPublish tests
    Issue #255

--- test/unit/terrareg/server/test_api_terrareg_module_version_publish.py
+++ test/unit/terrareg/server/test_api_terrareg_module_version_publish.py
@@ -111,7 +111,8 @@ class TestApiModuleVersionPublish(TerraregUnitTest):
 
             res = client.post(
                 '/v1/terrareg/modules/testnamespace/modulenotpublished/testprovider/10.2.1/publish')
-            assert res.status_code == 401
-            assert res.json == {'message': 'The server could not verify that you are authorized to access the URL requested. You either supplied the wrong credentials (e.g. a bad password), or your browser doesn\'t understand how to supply the credentials required.'}
+            assert res.status_code == 403
+            assert res.json == {'message': "You don't have the permission to access the requested resource. "
+                                           "It is either read-protected or not readable by the server."}
 
             mocked_update_attributes.assert_not_called()
