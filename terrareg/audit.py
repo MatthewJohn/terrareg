5132e4615a1d272c345348d2f2ccc9bd3332cd78
Matthew John <matthew@dockstudios.co.uk>
2022-11-22 17:22:14 +0000

    feat: Implement audit log querying, limit+offset, sorting and ordering in API and datatable
    Issue #253

--- terrareg/audit.py
+++ terrareg/audit.py
@@ -10,18 +10,46 @@ import terrareg.auth
 class AuditEvent:
     
     @classmethod
-    def get_events(cls):
+    def get_events(cls, limit=10, offset=0, descending=True,
+                    order_by='timestamp', query=None):
         """Return audit events from database"""
         db = Database.get()
-        query = sqlalchemy.select(
+        db_query = sqlalchemy.select(
             db.audit_history
         ).select_from(
             db.audit_history
         )
+        filtered = db_query
+        if query:
+            filtered = filtered.where(
+                sqlalchemy.or_(
+                    db.audit_history.c.username.like(f'%{query}%'),
+                    db.audit_history.c.action.like(f'%{query}%'),
+                    db.audit_history.c.object_id.like(f'%{query}%'),
+                    db.audit_history.c.old_value.like(f'%{query}%'),
+                    db.audit_history.c.new_value.like(f'%{query}%')
+                )
+            )
+        filtered_limit = filtered.order_by(
+            sqlalchemy.desc(order_by) if descending else sqlalchemy.asc(order_by)
+        ).limit(
+            limit
+        ).offset(
+            offset
+        )
+        filtered_count_query = sqlalchemy.select(
+            sqlalchemy.func.count('*').label('count')
+        ).select_from(filtered)
+        total_count_query = sqlalchemy.select(
+            sqlalchemy.func.count('*').label('count')
+        ).select_from(db_query)
 
         with db.get_connection() as conn:
-            res = conn.execute(query).fetchall()
-        return res
+            res = conn.execute(filtered_limit)
+            res = res.fetchall()
+            filtered_count = conn.execute(filtered_count_query).fetchone()['count']
+            total_count = conn.execute(total_count_query).fetchone()['count']
+        return res, total_count, filtered_count
 
     @classmethod
     def create_audit_event(cls, action,
