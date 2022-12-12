6d6395c1e22db39f953e33bc3cb98caebf40f6e4
Matthew John <matthew@dockstudios.co.uk>
2022-12-12 15:51:20 +0000

    test: Add test for column ordering in audit page
    Issue #253

--- test/selenium/test_audit_history.py
+++ test/selenium/test_audit_history.py
@@ -19,45 +19,54 @@ class TestAuditHistory(SeleniumTest):
 
     _SECRET_KEY = '354867a669ef58d17d0513a0f3d02f4403354915139422a8931661a3dbccdffe'
 
+    _AUDIT_DATA = {
+        'user_group_delete': ['useradmin', AuditAction.USER_GROUP_DELETE, 'UserGroup', 'test-user-group', None, None,
+                 datetime(year=2099, month=1, day=2, hour=9, minute=2, second=0)],
+        'user_group_create': ['useradmin', AuditAction.USER_GROUP_CREATE, 'UserGroup', 'test-user-group', None, None,
+                 datetime(year=2099, month=1, day=2, hour=9, minute=1, second=0)],
+        'namespace_create': [
+            'test-event-admin', AuditAction.NAMESPACE_CREATE, 'Namespace', 'test-namespace', None, None,
+            datetime(year=2020, month=11, day=27, hour=19, minute=14, second=10)
+        ],
+        'module_provider_create': ['test-event-admin', AuditAction.NAMESPACE_CREATE, 'Namespace', 'test-namespace', None, None,
+                 datetime(year=2020, month=11, day=27, hour=19, minute=14, second=10)],
+        'module_version_index_1': ['namespaceowner', AuditAction.MODULE_VERSION_INDEX, 'Module', 'test-namespace/test-module/2.0.1', None, None,
+                 datetime(year=2021, month=12, day=28, hour=19, minute=16, second=23)],
+        'module_version_index_2': ['namespaceowner', AuditAction.MODULE_VERSION_INDEX, 'Module', 'test-namespace/test-module/2.0.1', None, None,
+                 datetime(year=2021, month=12, day=28, hour=19, minute=15, second=10)],
+        'module_version_publish': ['namespaceowner', AuditAction.MODULE_VERSION_PUBLISH, 'Module', 'test-namespace/test-module/2.0.1', None, None,
+                 datetime(year=2021, month=12, day=29, hour=19, minute=23, second=31)],
+        'module_version_delete': ['namespaceowner', AuditAction.MODULE_VERSION_DELETE, 'Module', 'test-namespace/test-module/2.0.1', None, None,
+                 datetime(year=2021, month=12, day=29, hour=20, minute=12, second=23)],
+        'user_login_1': ['testuser1', AuditAction.USER_LOGIN, 'User', 'testuser1', None, None,
+                 datetime(year=2020, month=1, day=2, hour=9, minute=49, second=20)],
+        'user_login_2': ['testuser2', AuditAction.USER_LOGIN, 'User', 'testuser2', None, None,
+                 datetime(year=2020, month=1, day=2, hour=9, minute=50, second=20)],
+        'user_login_3': ['testuser3', AuditAction.USER_LOGIN, 'User', 'testuser4', None, None,
+                 datetime(year=2020, month=1, day=2, hour=9, minute=52, second=20)],
+        'user_login_4': ['testuser4', AuditAction.USER_LOGIN, 'User', 'testuser4', None, None,
+                 datetime(year=2020, month=1, day=2, hour=9, minute=53, second=20)],
+        'user_login_5': ['testuser5', AuditAction.USER_LOGIN, 'User', 'testuser5', None, None,
+                 datetime(year=2020, month=1, day=2, hour=9, minute=54, second=20)],
+        'user_login_6': ['testuser6', AuditAction.USER_LOGIN, 'User', 'testuser6', None, None,
+                 datetime(year=2020, month=1, day=2, hour=9, minute=55, second=20)],
+        'user_login_7': ['testuser7', AuditAction.USER_LOGIN, 'User', 'testuser7', None, None,
+                 datetime(year=2020, month=1, day=2, hour=9, minute=56, second=20)],
+        'user_login_8': ['testuser8', AuditAction.USER_LOGIN, 'User', 'testuser8', None, None,
+                 datetime(year=2020, month=1, day=2, hour=9, minute=57, second=20)],
+        'user_login_9': ['testuser9', AuditAction.USER_LOGIN, 'User', 'testuser9', None, None,
+                 datetime(year=2020, month=1, day=2, hour=9, minute=58, second=20)],
+        'user_login_10': ['testuser10', AuditAction.USER_LOGIN, 'User', 'testuser10', None, None,
+                 datetime(year=2020, month=1, day=2, hour=9, minute=59, second=20)]
+        
+    }
+
     @classmethod
     def _setup_test_audit_data(cls):
         """Setup test audit data."""
         db = Database.get()
         with db.get_connection() as conn:
-            for data in [
-                ['test-event-admin', AuditAction.NAMESPACE_CREATE, 'Namespace', 'test-namespace', None, None,
-                 datetime(year=2020, month=11, day=27, hour=19, minute=14, second=10)],
-                ['test-event-admin', AuditAction.MODULE_PROVIDER_CREATE, 'Module', 'test-namespace/test-module', None, None,
-                 datetime(year=2021, month=11, day=27, hour=19, minute=15, second=10)],
-                ['test-event-admin', AuditAction.MODULE_VERSION_INDEX, 'Module', 'test-namespace/test-module/2.0.1', None, None,
-                 datetime(year=2021, month=12, day=28, hour=19, minute=16, second=23)],
-                ['test-event-admin', AuditAction.MODULE_VERSION_INDEX, 'Module', 'test-namespace/test-module/2.0.1', None, None,
-                 datetime(year=2021, month=12, day=28, hour=19, minute=15, second=10)],
-                ['test-event-admin', AuditAction.MODULE_VERSION_PUBLISH, 'Module', 'test-namespace/test-module/2.0.1', None, None,
-                 datetime(year=2021, month=12, day=29, hour=19, minute=23, second=31)],
-                ['test-event-admin', AuditAction.MODULE_VERSION_DELETE, 'Module', 'test-namespace/test-module/2.0.1', None, None,
-                 datetime(year=2021, month=12, day=29, hour=20, minute=12, second=23)],
-                ['test-event-admin', AuditAction.USER_LOGIN, 'User', 'testuser1', None, None,
-                 datetime(year=2020, month=1, day=2, hour=9, minute=49, second=20)],
-                ['test-event-admin', AuditAction.USER_LOGIN, 'User', 'testuser2', None, None,
-                 datetime(year=2020, month=1, day=2, hour=9, minute=50, second=20)],
-                ['test-event-admin', AuditAction.USER_LOGIN, 'User', 'testuser3', None, None,
-                 datetime(year=2020, month=1, day=2, hour=9, minute=51, second=20)],
-                ['test-event-admin', AuditAction.USER_LOGIN, 'User', 'testuser4', None, None,
-                 datetime(year=2020, month=1, day=2, hour=9, minute=52, second=20)],
-                ['test-event-admin', AuditAction.USER_LOGIN, 'User', 'testuser4', None, None,
-                 datetime(year=2020, month=1, day=2, hour=9, minute=53, second=20)],
-                ['test-event-admin', AuditAction.USER_LOGIN, 'User', 'testuser5', None, None,
-                 datetime(year=2020, month=1, day=2, hour=9, minute=54, second=20)],
-                ['test-event-admin', AuditAction.USER_LOGIN, 'User', 'testuser6', None, None,
-                 datetime(year=2020, month=1, day=2, hour=9, minute=55, second=20)],
-                ['test-event-admin', AuditAction.USER_LOGIN, 'User', 'testuser7', None, None,
-                 datetime(year=2020, month=1, day=2, hour=9, minute=56, second=20)],
-                ['test-event-admin', AuditAction.USER_LOGIN, 'User', 'testuser8', None, None,
-                 datetime(year=2020, month=1, day=2, hour=9, minute=57, second=20)],
-                ['test-event-admin', AuditAction.USER_LOGIN, 'User', 'testuser9', None, None,
-                 datetime(year=2020, month=1, day=2, hour=9, minute=58, second=20)],
-            ]:
+            for data in cls._AUDIT_DATA.values():
                 conn.execute(
                     db.audit_history.insert().values(
                         username=data[0],
@@ -69,7 +78,6 @@ class TestAuditHistory(SeleniumTest):
                         timestamp=data[6]
                     )
                 )
-
 
     @classmethod
     def setup_class(cls):
@@ -102,12 +110,19 @@ class TestAuditHistory(SeleniumTest):
 
         assert self.selenium_instance.current_url == self.get_url('/audit-history')
 
-    def _ensure_audit_row_is_like(self, row, timestamp, username, action, object_id, old_value, new_value):
+    def _ensure_audit_row_is_like(self, row, audit_item):
         """Ensure row matches expected data"""
         assert [
             td.text
             for td in row.find_elements(By.TAG_NAME, 'td')
-        ] == [timestamp, username, action, object_id, old_value, new_value]
+        ] == [
+            audit_item[6].isoformat(),
+            audit_item[0],
+            audit_item[1].name,
+            audit_item[3],
+            audit_item[4] if audit_item[4] else '',
+            audit_item[5] if audit_item[5] else ''
+        ]
 
     def _get_audit_rows(self, parent):
         """Return rows from table"""
@@ -133,13 +148,11 @@ class TestAuditHistory(SeleniumTest):
         # Ignore header row and first audit event caused by login
         self._ensure_audit_row_is_like(
             rows[2],
-            timestamp='2021-11-27T20:12:23', username='test-event-admin',
-            action='MODULE_VERSION_DELETE', object_id='test-namespace/test-module/2.0.1', old_value='', new_value='')
+            self._AUDIT_DATA['module_version_delete'])
 
         self._ensure_audit_row_is_like(
             rows[3],
-            timestamp='2021-11-27T19:23:31', username='test-event-admin',
-            action='MODULE_VERSION_PUBLISH', object_id='test-namespace/test-module/2.0.1', old_value='', new_value='')
+            self._AUDIT_DATA['module_version_publish'])
 
     def test_pagination(self):
         """Test pagination for audit history."""
@@ -161,13 +174,11 @@ class TestAuditHistory(SeleniumTest):
         # Check content of rows
         self._ensure_audit_row_is_like(
             rows[1],
-            timestamp='2020-01-02T09:49:20', username='test-event-admin',
-            action='USER_LOGIN', object_id='testuser1', old_value='', new_value='')
+            self._AUDIT_DATA['user_login_1'])
 
         self._ensure_audit_row_is_like(
             rows[2],
-            timestamp='2020-01-02T09:50:20', username='test-event-admin',
-            action='USER_LOGIN', object_id='testuser2', old_value='', new_value='')
+            self._AUDIT_DATA['user_login_2'])
 
         # Ensure previous is disabled and next is available
         # Temporarily disabled due to is_enabled returning True
@@ -181,25 +192,69 @@ class TestAuditHistory(SeleniumTest):
         # Click next page
         self.selenium_instance.find_element(By.ID, 'audit-history-table_next').find_element(By.TAG_NAME, 'a').click()
 
-        # Ensure that there are 7 rows plus header
-        self.assert_equals(lambda: len(self._get_audit_rows(audit_table)), 8)
+        # Ensure that there are 9 rows plus header
+        self.assert_equals(lambda: len(self._get_audit_rows(audit_table)), 10)
 
         rows = self._get_audit_rows(audit_table)
 
         # Check content of rows
         self._ensure_audit_row_is_like(
             rows[1],
-            timestamp='2020-11-27T19:14:10', username='test-event-admin',
-            action='NAMESPACE_CREATE', object_id='test-namespace', old_value='', new_value='')
+            self._AUDIT_DATA['namespace_create'])
 
         self._ensure_audit_row_is_like(
-            rows[2],
-            timestamp='2021-11-27T19:15:10', username='test-event-admin',
-            action='MODULE_PROVIDER_CREATE', object_id='test-namespace/test-module', old_value='', new_value='')
+            rows[1],
+            self._AUDIT_DATA['module_provider_create'])
 
     def test_column_ordering(self):
         """Test ordering data by column."""
-        pass
+        self.perform_admin_authentication('unittest-password')
+
+        # Load homepage, waiting for drop-down to be rendered and navigate to audit history page
+        self.selenium_instance.get(self.get_url('/audit-history'))
+
+        assert self.selenium_instance.find_element(By.CLASS_NAME, 'breadcrumb').text == 'Audit History'
+
+        # Ensure table is shown and fields are present
+        audit_table = self.selenium_instance.find_element(By.ID, 'audit-history-table')
+
+        # Ensure only 10 rows are shown in table (plus heading)
+        self.assert_equals(lambda: len(self._get_audit_rows(audit_table)), 11)
+
+        rows = self._get_audit_rows(audit_table)
+
+        column_headers = [r for r in audit_table.find_elements(By.TAG_NAME, 'th')]
+        for column_itx, expected_rows in [
+            # Sort by timestamp, reversed
+            (0, [self._AUDIT_DATA['user_group_delete'], self._AUDIT_DATA['user_group_create']]),
+
+            # Sort by username
+            (1, [None, self._AUDIT_DATA['module_version_index_1']]),
+            # Sort by username reversed
+            (1, [self._AUDIT_DATA['user_group_delete'], self._AUDIT_DATA['user_group_create']]),
+
+            # Sort by action
+            (2, [None, self._AUDIT_DATA['module_version_index_1']]),
+            # Sort by action reversed
+            (2, [self._AUDIT_DATA['user_login_1'], self._AUDIT_DATA['user_login_2']]),
+
+            # Sort by object
+            (3, [None, self._AUDIT_DATA['namespace_create']]),
+            # Sort by object reversed
+            (3, [self._AUDIT_DATA['user_login_9'], self._AUDIT_DATA['user_login_8']])
+        ]:
+            print(f'Testing column sorting: {column_itx}')
+
+            # Click header
+            column_headers[column_itx].click()
+            rows = self._get_audit_rows(audit_table)
+            # Check rows
+            for itx, row_test in enumerate(expected_rows):
+                if row_test is not None:
+                    self._ensure_audit_row_is_like(
+                        rows[itx + 1],
+                        row_test
+                    )
 
     def test_result_filtering(self):
         """Test filtering results using query string."""
