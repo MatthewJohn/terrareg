272a8d55af167098d480bffa8e30a4c090648032
Matthew John <matthew@dockstudios.co.uk>
2023-01-09 18:43:37 +0000

    Re-add debug for image comparison
    Issue #286

--- test/selenium/test_module_provider.py
+++ test/selenium/test_module_provider.py
@@ -2109,6 +2109,16 @@ All rights are not reserved for this example file content</pre>
         expected_image = Image.open(compare_filename)
         expected_image = expected_image.crop(expected_image.getbbox())
 
+        print(imagehash.phash(expected_image))
+        print(imagehash.phash(actual_image))
+        print(imagehash.whash(expected_image))
+        print(imagehash.whash(actual_image))
+        print(imagehash.average_hash(expected_image))
+        print(imagehash.average_hash(actual_image))
+        print(imagehash.phash(expected_image))
+        print(imagehash.phash(actual_image))
+        print("END")
+
         return imagehash.phash(actual_image) == imagehash.phash(expected_image)
 
     @pytest.mark.parametrize("base_url,expected_url,base_filename,", [
