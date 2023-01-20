1be14ee7f9a0472b4c6621a4869ea3ac5cc1c6ce
Matthew John <matthew@dockstudios.co.uk>
2023-01-20 08:00:41 +0000

    feat: Add alternative themes with user preferences option to select theme.
    Add function to load CSS dynamically based on stored user preferences.
    Update modal methods to store/load themes from local storage.
    
    Issue #309

--- terrareg/static/js/terrareg/user-preferences.js
+++ terrareg/static/js/terrareg/user-preferences.js
@@ -15,7 +15,8 @@ function getLocalStorageValue(key, defaultValue, type) {
 function getUserPreferences() {
     return {
         'show-beta-versions': getLocalStorageValue('show-beta-versions', false, Boolean),
-        'show-unpublished-versions': getLocalStorageValue('show-unpublished-versions', false, Boolean)
+        'show-unpublished-versions': getLocalStorageValue('show-unpublished-versions', false, Boolean),
+        'theme': getLocalStorageValue('theme', 'default', String)
     }
 }
 
@@ -23,6 +24,7 @@ function userPreferencesModalShow() {
     let currentPreferences = getUserPreferences();
     $('#user-preferences-show-beta').prop('checked', currentPreferences["show-beta-versions"]);
     $('#user-preferences-show-unpublished').prop('checked', currentPreferences["show-unpublished-versions"]);
+    $('#user-preferences-theme').val(currentPreferences["theme"]);
 
     $('#user-preferences-modal')[0].classList.add('is-active');
 }
@@ -30,9 +32,29 @@ function userPreferencesModalShow() {
 function userPreferencesModalSave() {
     localStorage.setItem('show-beta-versions', $('#user-preferences-show-beta').is(':checked'));
     localStorage.setItem('show-unpublished-versions', $('#user-preferences-show-unpublished').is(':checked'));
+    localStorage.setItem('theme', $('#user-preferences-theme').val());
     userPreferencesModalClose();
 }
 
 function userPreferencesModalClose() {
     $('#user-preferences-modal')[0].classList.remove('is-active');
 }
+
+function setCurrentTheme() {
+    // Load theme CSS
+    let userPreferences = getUserPreferences();
+    let url = `/static/css/bulma/${userPreferences['theme']}/bulmaswatch.min.css`;
+    if (userPreferences['theme'] == 'default') {
+      url = '/static/css/bulma/bulma-0.9.3.min.css';
+    }
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = 'bulma';
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = url;
+    link.media = 'all';
+    head.appendChild(link);
+}
+
+setCurrentTheme();
