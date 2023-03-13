5ac88f0b3cedbd1bdbc339909e2af189669f5154
Matthew John <matthew@dockstudios.co.uk>
2023-03-13 16:47:56 +0000

    Remove transaction around bitbucket/github index endpoints, since module version creation handles the deletion during an extraction failure.
    This also caused errors due to nested transaction during finalise_module
    
    Issue #332

--- terrareg/server/api/module_version_create_bitbucket_hook.py
+++ terrareg/server/api/module_version_create_bitbucket_hook.py
@@ -19,104 +19,97 @@ class ApiModuleVersionCreateBitBucketHook(ErrorCatchingResource):
 
     def _post(self, namespace, name, provider):
         """Create new version based on bitbucket hooks."""
-        with terrareg.database.Database.start_transaction() as transaction_context:
-            _, _, module_provider, error = self.get_module_provider_by_names(namespace, name, provider)
-            if error:
-                return error
+        _, _, module_provider, error = self.get_module_provider_by_names(namespace, name, provider)
+        if error:
+            return error
 
-            # Validate signature
-            if terrareg.config.Config().UPLOAD_API_KEYS:
-                # Get signature from request
-                request_signature = request.headers.get('X-Hub-Signature', '')
-                # Remove 'sha256=' from beginning of header
-                request_signature = re.sub(r'^sha256=', '', request_signature)
-                # Iterate through each of the keys and test
-                for test_key in terrareg.config.Config().UPLOAD_API_KEYS:
-                    # Generate
-                    valid_signature = hmac.new(bytes(test_key, 'utf8'), b'', hashlib.sha256)
-                    valid_signature.update(request.data)
-                    # If the signatures match, break from loop
-                    if hmac.compare_digest(valid_signature.hexdigest(), request_signature):
-                        break
-                # If a valid signature wasn't found with one of the configured keys,
-                # return 401
-                else:
-                    return self._get_401_response()
+        # Validate signature
+        if terrareg.config.Config().UPLOAD_API_KEYS:
+            # Get signature from request
+            request_signature = request.headers.get('X-Hub-Signature', '')
+            # Remove 'sha256=' from beginning of header
+            request_signature = re.sub(r'^sha256=', '', request_signature)
+            # Iterate through each of the keys and test
+            for test_key in terrareg.config.Config().UPLOAD_API_KEYS:
+                # Generate
+                valid_signature = hmac.new(bytes(test_key, 'utf8'), b'', hashlib.sha256)
+                valid_signature.update(request.data)
+                # If the signatures match, break from loop
+                if hmac.compare_digest(valid_signature.hexdigest(), request_signature):
+                    break
+            # If a valid signature wasn't found with one of the configured keys,
+            # return 401
+            else:
+                return self._get_401_response()
 
-            if not module_provider.get_git_clone_url():
-                return {'message': 'Module provider is not configured with a repository'}, 400
+        if not module_provider.get_git_clone_url():
+            return {'message': 'Module provider is not configured with a repository'}, 400
 
-            bitbucket_data = request.json
+        bitbucket_data = request.json
 
-            if not ('changes' in bitbucket_data and type(bitbucket_data['changes']) == list):
-                return {'message': 'List of changes not found in payload'}, 400
+        if not ('changes' in bitbucket_data and type(bitbucket_data['changes']) == list):
+            return {'message': 'List of changes not found in payload'}, 400
 
-            imported_versions = {}
-            error = False
+        imported_versions = {}
+        error = False
 
-            for change in bitbucket_data['changes']:
+        for change in bitbucket_data['changes']:
 
-                # Check that change is a dict
-                if not type(change) is dict:
-                    continue
+            # Check that change is a dict
+            if not type(change) is dict:
+                continue
 
-                # Check if change type is tag
-                if not ('ref' in change and
-                        type(change['ref']) is dict and
-                        'type' in change['ref'] and
-                        type(change['ref']['type']) == str and
-                        change['ref']['type'] == 'TAG'):
-                    continue
+            # Check if change type is tag
+            if not ('ref' in change and
+                    type(change['ref']) is dict and
+                    'type' in change['ref'] and
+                    type(change['ref']['type']) == str and
+                    change['ref']['type'] == 'TAG'):
+                continue
 
-                # Check type of change is an ADD or UPDATE
-                if not ('type' in change and
-                        type(change['type']) is str and
-                        change['type'] in ['ADD', 'UPDATE']):
-                    continue
+            # Check type of change is an ADD or UPDATE
+            if not ('type' in change and
+                    type(change['type']) is str and
+                    change['type'] in ['ADD', 'UPDATE']):
+                continue
 
-                # Obtain tag name
-                tag_ref = change['ref']['id'] if 'id' in change['ref'] else None
+            # Obtain tag name
+            tag_ref = change['ref']['id'] if 'id' in change['ref'] else None
 
-                # Attempt to match version against regex
-                version = module_provider.get_version_from_tag_ref(tag_ref)
+            # Attempt to match version against regex
+            version = module_provider.get_version_from_tag_ref(tag_ref)
 
-                if not version:
-                    continue
+            if not version:
+                continue
 
-                # Create module version
-                module_version = terrareg.models.ModuleVersion(module_provider=module_provider, version=version)
+            # Create module version
+            module_version = terrareg.models.ModuleVersion(module_provider=module_provider, version=version)
 
-                # Perform import from git
-                savepoint = transaction_context.connection.begin_nested()
-                try:
-                    with terrareg.module_version_create.module_version_create(module_version):
-                        with terrareg.module_extractor.GitModuleExtractor(module_version=module_version) as me:
-                            me.process_upload()
+            # Perform import from git
+            try:
+                with terrareg.module_version_create.module_version_create(module_version):
+                    with terrareg.module_extractor.GitModuleExtractor(module_version=module_version) as me:
+                        me.process_upload()
 
-                except terrareg.errors.TerraregError as exc:
-                    # Roll back the transaction for this module version
-                    savepoint.rollback()
+            except terrareg.errors.TerraregError as exc:
+                imported_versions[version] = {
+                    'status': 'Failed',
+                    'message': str(exc)
+                }
+                error = True
+            else:
+                imported_versions[version] = {
+                    'status': 'Success'
+                }
 
-                    imported_versions[version] = {
-                        'status': 'Failed',
-                        'message': str(exc)
-                    }
-                    error = True
-                else:
-                    # Commit the transaction for this version
-                    savepoint.commit()
-                    imported_versions[version] = {
-                        'status': 'Success'
-                    }
-
-            if error:
-                return {
-                    'status': 'Error',
-                    'message': 'One or more tags failed to import',
-                    'tags': imported_versions
-                }, 500
+        if error:
             return {
-                'status': 'Success',
-                'message': 'Imported all provided tags',
+                'status': 'Error',
+                'message': 'One or more tags failed to import',
                 'tags': imported_versions
-            }
+            }, 500
+        return {
+            'status': 'Success',
+            'message': 'Imported all provided tags',
+            'tags': imported_versions
+        }
--- terrareg/server/api/module_version_create_github_hook.py
+++ terrareg/server/api/module_version_create_github_hook.py
@@ -19,89 +19,85 @@ class ApiModuleVersionCreateGitHubHook(ErrorCatchingResource):
 
     def _post(self, namespace, name, provider):
         """Create, update or delete new version based on GitHub release hooks."""
-        with terrareg.database.Database.start_transaction() as transaction_context:
-            _, _, module_provider, error = self.get_module_provider_by_names(namespace, name, provider)
-            if error:
-                return error
+        _, _, module_provider, error = self.get_module_provider_by_names(namespace, name, provider)
+        if error:
+            return error
 
-            # Validate signature
-            if terrareg.config.Config().UPLOAD_API_KEYS:
-                # Get signature from request
-                request_signature = request.headers.get('X-Hub-Signature', '')
-                # Remove 'sha256=' from beginning of header
-                request_signature = re.sub(r'^sha256=', '', request_signature)
-                # Iterate through each of the keys and test
-                for test_key in terrareg.config.Config().UPLOAD_API_KEYS:
-                    # Generate
-                    valid_signature = hmac.new(bytes(test_key, 'utf8'), b'', hashlib.sha256)
-                    valid_signature.update(request.data)
-                    # If the signatures match, break from loop
-                    if hmac.compare_digest(valid_signature.hexdigest(), request_signature):
-                        break
-                # If a valid signature wasn't found with one of the configured keys,
-                # return 401
-                else:
-                    return self._get_401_response()
-
-            if not module_provider.get_git_clone_url():
-                return {'status': 'Error', 'message': 'Module provider is not configured with a repository'}, 400
-
-            github_data = request.json
-
-            if not ('release' in github_data and type(github_data['release']) == dict):
-                return {'status': 'Error', 'message': 'Received a non-release hook request'}, 400
-
-            release = github_data['release']
-    
-            # Obtain tag name
-            tag_ref = release.get('tag_name')
-            if not tag_ref:
-                return {'status': 'Error', 'message': 'tag_name not present in request'}, 400
-
-            # Attempt to match version against regex
-            version = module_provider.get_version_from_tag(tag_ref)
-
-            if not version:
-                return {'status': 'Error', 'message': 'Release tag does not match configured version regex'}, 400
-
-            # Create module version
-            module_version = terrareg.models.ModuleVersion(module_provider=module_provider, version=version)
-
-            action = github_data.get('action')
-            if not action:
-                return {"status": "Error", "message": "No action present in request"}, 400
-
-            if action in ['deleted', 'unpublished']:
-                if not terrareg.config.Config().UPLOAD_API_KEYS:
-                    return {
-                        'status': 'Error',
-                        'message': 'Version deletion requires API key authentication',
-                        'tag': tag_ref
-                    }, 400
-                module_version.delete()
-
-                return {
-                    'status': 'Success'
-                }
+        # Validate signature
+        if terrareg.config.Config().UPLOAD_API_KEYS:
+            # Get signature from request
+            request_signature = request.headers.get('X-Hub-Signature', '')
+            # Remove 'sha256=' from beginning of header
+            request_signature = re.sub(r'^sha256=', '', request_signature)
+            # Iterate through each of the keys and test
+            for test_key in terrareg.config.Config().UPLOAD_API_KEYS:
+                # Generate
+                valid_signature = hmac.new(bytes(test_key, 'utf8'), b'', hashlib.sha256)
+                valid_signature.update(request.data)
+                # If the signatures match, break from loop
+                if hmac.compare_digest(valid_signature.hexdigest(), request_signature):
+                    break
+            # If a valid signature wasn't found with one of the configured keys,
+            # return 401
             else:
-                # Perform import from git
-                try:
-                    with terrareg.module_version_create.module_version_create(module_version):
-                        with terrareg.module_extractor.GitModuleExtractor(module_version=module_version) as me:
-                            me.process_upload()
+                return self._get_401_response()
 
-                except terrareg.errors.TerraregError as exc:
-                    # Roll back creation of module version
-                    transaction_context.transaction.rollback()
+        if not module_provider.get_git_clone_url():
+            return {'status': 'Error', 'message': 'Module provider is not configured with a repository'}, 400
 
-                    return {
-                        'status': 'Error',
-                        'message': 'Tag failed to import',
-                        'tag': tag_ref
-                    }, 500
-                else:
-                    return {
-                        'status': 'Success',
-                        'message': 'Imported provided tag',
-                        'tag': tag_ref
-                    }
+        github_data = request.json
+
+        if not ('release' in github_data and type(github_data['release']) == dict):
+            return {'status': 'Error', 'message': 'Received a non-release hook request'}, 400
+
+        release = github_data['release']
+
+        # Obtain tag name
+        tag_ref = release.get('tag_name')
+        if not tag_ref:
+            return {'status': 'Error', 'message': 'tag_name not present in request'}, 400
+
+        # Attempt to match version against regex
+        version = module_provider.get_version_from_tag(tag_ref)
+
+        if not version:
+            return {'status': 'Error', 'message': 'Release tag does not match configured version regex'}, 400
+
+        # Create module version
+        module_version = terrareg.models.ModuleVersion(module_provider=module_provider, version=version)
+
+        action = github_data.get('action')
+        if not action:
+            return {"status": "Error", "message": "No action present in request"}, 400
+
+        if action in ['deleted', 'unpublished']:
+            if not terrareg.config.Config().UPLOAD_API_KEYS:
+                return {
+                    'status': 'Error',
+                    'message': 'Version deletion requires API key authentication',
+                    'tag': tag_ref
+                }, 400
+            module_version.delete()
+
+            return {
+                'status': 'Success'
+            }
+        else:
+            # Perform import from git
+            try:
+                with terrareg.module_version_create.module_version_create(module_version):
+                    with terrareg.module_extractor.GitModuleExtractor(module_version=module_version) as me:
+                        me.process_upload()
+
+            except terrareg.errors.TerraregError as exc:
+                return {
+                    'status': 'Error',
+                    'message': 'Tag failed to import',
+                    'tag': tag_ref
+                }, 500
+            else:
+                return {
+                    'status': 'Success',
+                    'message': 'Imported provided tag',
+                    'tag': tag_ref
+                }
