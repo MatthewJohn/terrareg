e911aa878897526d06dde4c8ad0de96f77d08d7b
Matthew John <matthew@dockstudios.co.uk>
2023-03-28 07:20:50 +0100

    fix: Reload current page when any user preferences are changed
    Issue #376

--- terrareg/static/js/terrareg/user-preferences.js
+++ terrareg/static/js/terrareg/user-preferences.js
@@ -30,18 +30,13 @@ function userPreferencesModalShow() {
 }
 
 function userPreferencesModalSave() {
-    let newTheme = $('#user-preferences-theme').val();
-    let themeHasChanged = newTheme !== getUserPreferences().theme;
-
     localStorage.setItem('show-beta-versions', $('#user-preferences-show-beta').is(':checked'));
     localStorage.setItem('show-unpublished-versions', $('#user-preferences-show-unpublished').is(':checked'));
     localStorage.setItem('theme', $('#user-preferences-theme').val());
-    $.cookie("theme", newTheme, {path: "/"});
+    $.cookie("theme", $('#user-preferences-theme').val(), {path: "/"});
     userPreferencesModalClose();
 
-    if (themeHasChanged) {
-        location.reload();
-    }
+    location.reload();
 }
 
 function userPreferencesModalClose() {
