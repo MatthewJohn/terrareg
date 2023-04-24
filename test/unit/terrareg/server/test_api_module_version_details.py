4605a2bfc00823539608e16e80d35e9a48d1ad12
Matthew John <matthew@dockstudios.co.uk>
2023-04-24 06:03:22 +0100

    test: Update module version details tests to use populated module, testing the terraform attributes
    Issue #139

--- test/unit/terrareg/server/test_api_module_version_details.py
+++ test/unit/terrareg/server/test_api_module_version_details.py
@@ -12,23 +12,48 @@ class TestApiModuleVersionDetails(TerraregUnitTest):
 
     @setup_test_data()
     def test_existing_module_version(self, client, mock_models):
-        res = client.get('/v1/modules/testnamespace/testmodulename/testprovider/1.0.0')
+        res = client.get('/v1/modules/moduledetails/fullypopulated/testprovider/1.5.0')
 
         assert res.json == {
-            'id': 'testnamespace/testmodulename/testprovider/1.0.0', 'owner': 'Mock Owner',
-            'namespace': 'testnamespace', 'name': 'testmodulename',
-            'version': '1.0.0', 'provider': 'testprovider',
-            'description': 'Mock description',
-            'source': 'http://mock.example.com/mockmodule',
-            'published_at': '2020-01-01T23:18:12',
-            'downloads': 0, 'verified': True, 'trusted': False, 'internal': False,
+            'id': 'moduledetails/fullypopulated/testprovider/1.5.0',
+            'namespace': 'moduledetails',
+            'name': 'fullypopulated',
+            'provider': 'testprovider',
+            'verified': False,
+            'trusted': False,
+            'versions': ['1.2.0', '1.6.1-beta', '1.5.0'],
+            'owner': 'This is the owner of the module',
+            'version': '1.5.0',
+            'description': 'This is a test module version for tests.',
+            'source': 'https://link-to.com/source-code-here',
+            'published_at': '2022-01-05T22:53:12',
+            'downloads': 0,
+            'internal': False,
             'root': {
-                'path': '', 'readme': 'Mock module README file',
-                'empty': False, 'inputs': [], 'outputs': [], 'dependencies': [],
-                'provider_dependencies': [], 'resources': []
+                'path': '',
+                'readme': '# This is an exaple README!',
+                'empty': False,
+                'inputs': [
+                    {'name': 'name_of_application', 'type': 'string', 'description': 'Enter the application name', 'default': None, 'required': True},
+                    {'name': 'string_with_default_value', 'type': 'string', 'description': 'Override the default string', 'default': 'this is the default', 'required': False},
+                    {'name': 'example_boolean_input', 'type': 'bool', 'description': 'Override the truthful boolean', 'default': True, 'required': False},
+                    {'name': 'example_list_input', 'type': 'list', 'description': 'Override the stringy list', 'default': ['value 1', 'value 2'], 'required': False}
+                ],
+                'outputs': [
+                    {'name': 'generated_name', 'description': 'Name with randomness'},
+                    {'name': 'no_desc_output', 'description': None}
+                ],
+                'dependencies': [],
+                'provider_dependencies': [
+                    {'name': 'random', 'namespace': 'hashicorp', 'source': '', 'version': '>= 5.2.1, < 6.0.0'},
+                    {'name': 'unsafe', 'namespace': 'someothercompany', 'source': '', 'version': '2.0.0'}
+                ],
+                'resources': [
+                    {'type': 'string', 'name': 'random_suffix', 'provider': 'random', 'source': 'hashicorp/random', 'mode': 'managed', 'version': 'latest', 'description': None}
+                ]
             },
-            'submodules': [], 'providers': ['testprovider'],
-            'versions': ['2.4.1', '1.0.0']
+            'submodules': [],
+            'providers': ['testprovider']
         }
 
         assert res.status_code == 200
