88227b61076797a14d62e1b348c4c59ea637ec5b
Matthew John <matthew@dockstudios.co.uk>
2023-06-18 10:53:50 +0100

    test: Add unit test for _extract_example_files method and update Config tests for new config value
    Issue #303

--- test/unit/terrareg/test_module_extractor.py
+++ test/unit/terrareg/test_module_extractor.py
@@ -424,3 +424,120 @@ disable_checkpoint = true
 
             else:
                 assert not os.path.isfile(temp_file)
+
+
+    def test_extract_example_files(self):
+        """Test _extract_example_files method"""
+
+        test_module_dir = '/tmp/extraction_test'
+
+        tested_file_extensions = []
+        opened_files = []
+
+        pattern_responses = {
+            '*.tf': [
+                'subdirectory/main.tf',
+                'subdirectory/output.tf'
+            ],
+            '*.ext2': {},
+            '*.ext3': [
+                'subdirectory/blah.ext3'
+            ]
+        }
+        file_contents = {
+            'subdirectory/main.tf': 'test_content_main.tf',
+            'subdirectory/output.tf': 'output file content',
+            'subdirectory/blah.ext3': 'some ext3 content'
+        }
+
+        def mock_safe_iglob_effect(base_dir, pattern, recursive, is_file):
+            """Create mock iglob method to return list of files that match pattern
+            and mark the pattern as having been tested.
+            """
+            # Ensure base directory and other attributes are correct
+            assert base_dir == '/tmp/extraction_test/subdirectory'
+            assert recursive is False
+            assert is_file is True
+            # Ensure an expected pattern was provided
+            tested_file_extensions.append(pattern)
+            assert pattern in pattern_responses
+
+            # Return list of matching files
+            return [
+                f'{test_module_dir}/{filename}'
+                for filename in pattern_responses[pattern]
+            ]
+
+        mock_safe_iglob = unittest.mock.MagicMock(side_effect=mock_safe_iglob_effect)
+
+        def mock_open_file_effect(path, mode):
+            """Create mock side effect for open(), returning mock context manager.
+            The mock context manager with return a mock FH with mocked readlines() method
+            """
+            assert mode == 'r'
+            opened_files.append(path)
+            mock_open_context = unittest.mock.MagicMock()
+            mock_fh = unittest.mock.MagicMock()
+            mock_open_context.__enter__ = unittest.mock.MagicMock(return_value=mock_fh)
+            mock_fh.readlines = unittest.mock.MagicMock(
+                # Return file contents, removing the base directory from the path provided to open()
+                side_effect=lambda: file_contents[path.replace(f'{test_module_dir}/', '')]
+            )
+            return mock_open_context
+
+        mock_open_file = unittest.mock.MagicMock(side_effect=mock_open_file_effect)
+
+        mock_example = unittest.mock.MagicMock()
+        mock_example.path = './subdirectory'
+
+        created_example_files = {}
+
+        def mock_create_example_file(example, path):
+            """Mock ExampleFile.create to return mock instance of ExampleFile"""
+            assert example is mock_example
+            mock_example_file = unittest.mock.MagicMock()
+            created_example_files[path] = mock_example_file
+            return mock_example_file
+
+        # Create mock for ExampleFile and mock .create
+        mock_example_file = unittest.mock.MagicMock()
+        mock_example_file.create = unittest.mock.MagicMock(side_effect=mock_create_example_file)
+
+        # Create module version object with mocked git path,
+        # to allow mock.patch to read the previous property value
+        # during the mocking of GitModuleExtractor.module_directory
+        mock_module_version = unittest.mock.MagicMock()
+        mock_module_version.git_path = ''
+
+        with unittest.mock.patch('terrareg.module_extractor.Config.EXAMPLE_FILE_EXTENSIONS', ["tf", "ext2", "ext3"]), \
+                unittest.mock.patch('terrareg.module_extractor.open', mock_open_file), \
+                unittest.mock.patch('terrareg.module_extractor.safe_iglob', mock_safe_iglob), \
+                unittest.mock.patch('terrareg.module_extractor.GitModuleExtractor.module_directory', '/tmp/extraction_test'), \
+                unittest.mock.patch('terrareg.module_extractor.ExampleFile', mock_example_file):
+
+            module_extractor = GitModuleExtractor(module_version=mock_module_version)
+
+            module_extractor._extract_example_files(example=mock_example)
+
+        # Ensure each of the extensions was globbed for
+        assert tested_file_extensions == ['*.tf',  '*.ext2', '*.ext3']
+
+        # Ensure open() was called against each example file
+        assert opened_files == [
+            '/tmp/extraction_test/subdirectory/main.tf',
+            '/tmp/extraction_test/subdirectory/output.tf',
+            '/tmp/extraction_test/subdirectory/blah.ext3'
+        ]
+
+        # Ensure each returned file had an example created for
+        assert list(created_example_files.keys()) == [
+            'subdirectory/main.tf',
+            'subdirectory/output.tf',
+            'subdirectory/blah.ext3'
+        ]
+
+        # Ensure each example file was updated with correct content of file
+        for example_path, mock_example_file_instance in created_example_files.items():
+            mock_example_file_instance.update_attributes.assert_called_once_with(
+                content=file_contents[example_path]
+            )
