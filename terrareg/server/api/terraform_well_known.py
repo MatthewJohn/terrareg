46d501b73071414197cebf47cd79e679cdc7d125
Matthew John <matthew@dockstudios.co.uk>
2023-09-16 18:36:06 +0100

    chore: Only show oauth authentication in terraform wellknown when Terraform IDP is enabled
    Issue #289

--- terrareg/server/api/terraform_well_known.py
+++ terrareg/server/api/terraform_well_known.py
@@ -2,6 +2,7 @@
 from flask_restful import Resource
 
 from terrareg.constants import TERRAFORM_REDIRECT_URI_PORT_RANGE
+import terrareg.terraform_idp
 
 
 class ApiTerraformWellKnown(Resource):
@@ -9,13 +10,15 @@ class ApiTerraformWellKnown(Resource):
 
     def get(self):
         """Return wellknown JSON"""
-        return {
+        data = {
             "modules.v1": "/v1/modules/",
-            "login.v1": {
+        }
+        if terrareg.terraform_idp.TerraformIdp.get().is_enabled:
+            data["login.v1"] = {
                 "client": "terraform-cli",
                 "grant_types": ["authz_code", "token"],
                 "authz": "/terraform/oauth/authorization",
                 "token": "/terraform/oauth/token",
                 "ports": TERRAFORM_REDIRECT_URI_PORT_RANGE,
             }
-        }
+        return data
