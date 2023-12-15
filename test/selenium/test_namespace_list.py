c25240c2f66849e5895fba05f025b64d7454d8d7
Matthew John <matthew@dockstudios.co.uk>
2023-12-15 06:41:32 +0000

    test: Update namespace list tests to test modules and providers namespace list pages
    Issue #23

--- test/selenium/test_namespace_list.py
+++ test/selenium/test_namespace_list.py
@@ -11,9 +11,9 @@ from terrareg.models import ModuleVersion, Namespace, Module, ModuleProvider
 from .test_data import one_namespace_test_data
 
 class TestNamespaceList(SeleniumTest):
-    """Test namespace list page."""
+    """Test module namespace list page."""
 
-    def test_namespace_list_page(self):
+    def test_module_namespace_list_page(self):
         """Test namespace list page."""
         self.selenium_instance.get(self.get_url('/modules'))
 
@@ -73,9 +73,47 @@ class TestNamespaceList(SeleniumTest):
         self.selenium_instance.find_element(By.ID, 'nextButton').is_enabled() == False
         self.selenium_instance.find_element(By.ID, 'prevButton').is_enabled() == True
 
-    def test_namespace_list_page_with_unpublished(self):
+    def test_provider_namespace_list_page(self):
+        """Test namespace list page."""
+        self.selenium_instance.get(self.get_url('/providers'))
+
+        assert self.selenium_instance.title == 'Namespaces - Terrareg'
+
+        # Get content section
+        content = self.wait_for_element(By.ID, 'namespace-list-content')
+
+        # Check title
+        assert content.find_element(By.TAG_NAME, 'h1').text == 'Namespaces'
+
+        expected_namespaces = [
+            ['contributed-providersearch', 'contributed-providersearch'],
+            ['initial-providers', 'initial-providers'],
+            ['providersearch', 'providersearch'],
+            ['providersearch-trusted', 'providersearch-trusted'],
+            ['relevancysearch', 'relevancysearch'],
+            ['second-provider-namespace', 'second-provider-namespace'],
+        ]
+
+        # Check namespaces
+        table_body = content.find_element(By.ID, 'namespaces-table-data')
+        for namespace_tr in table_body.find_elements(By.TAG_NAME, 'tr'):
+            expected_name, expected_id = expected_namespaces.pop(0)
+
+            link = namespace_tr.find_element(By.TAG_NAME, 'a')
+            assert link.text == expected_name
+            assert link.get_attribute('href') == self.get_url(f'/providers/{expected_id}')
+
+        # Ensure previous link is not active and next link is
+        self.selenium_instance.find_element(By.ID, 'nextButton').is_enabled() == False
+        self.selenium_instance.find_element(By.ID, 'prevButton').is_enabled() == False
+
+    @pytest.mark.parametrize('base_url', [
+        '/modules',
+        '/providers'
+    ])
+    def test_namespace_list_page_with_unpublished(self, base_url):
         """Test namespace list page with unpublished selected."""
-        self.selenium_instance.get(self.get_url('/modules'))
+        self.selenium_instance.get(self.get_url(base_url))
 
         assert self.selenium_instance.title == 'Namespaces - Terrareg'
 
@@ -110,7 +148,7 @@ class TestNamespaceList(SeleniumTest):
 
             link = namespace_tr.find_element(By.TAG_NAME, 'a')
             assert link.text == expected_name
-            assert link.get_attribute('href') == self.get_url(f'/modules/{expected_id}')
+            assert link.get_attribute('href') == self.get_url(f'{base_url}/{expected_id}')
 
         # Ensure previous link is not active and next link is
         self.selenium_instance.find_element(By.ID, 'nextButton').is_enabled() == True
@@ -140,7 +178,7 @@ class TestNamespaceList(SeleniumTest):
 
             link = namespace_tr.find_element(By.TAG_NAME, 'a')
             assert link.text == expected_name
-            assert link.get_attribute('href') == self.get_url(f'/modules/{expected_id}')
+            assert link.get_attribute('href') == self.get_url(f'{base_url}/{expected_id}')
 
         # Ensure prev button is enabled and next button is not
         self.selenium_instance.find_element(By.ID, 'nextButton').is_enabled() == True
@@ -153,12 +191,16 @@ class TestNamespaceListSingleNamespace(SeleniumTest):
     _TEST_DATA = one_namespace_test_data
     _USER_GROUP_DATA = None
 
-    def test_namespace_list_page_redirect(self):
+    @pytest.mark.parametrize('base_url', [
+        '/modules',
+        '/providers'
+    ])
+    def test_namespace_list_page_redirect(self, base_url):
         """Test namespace list page with one namespace."""
-        self.selenium_instance.get(self.get_url('/modules'))
+        self.selenium_instance.get(self.get_url(base_url))
 
         # Ensure page is redirected to namespace page
-        self.assert_equals(lambda: self.selenium_instance.current_url, self.get_url('/modules/testnamespace'))
+        self.assert_equals(lambda: self.selenium_instance.current_url, self.get_url(f'{base_url}/testnamespace'))
 
 
 class TestNamespaceListNoNamespaces(SeleniumTest):
@@ -167,8 +209,12 @@ class TestNamespaceListNoNamespaces(SeleniumTest):
     _TEST_DATA = {}
     _USER_GROUP_DATA = None
 
-    def test_namespace_list_page_warning(self):
+    @pytest.mark.parametrize('base_url', [
+        '/modules',
+        '/providers'
+    ])
+    def test_namespace_list_page_warning(self, base_url):
         """Test namespace list page with no namespaces."""
-        self.selenium_instance.get(self.get_url('/modules'))
+        self.selenium_instance.get(self.get_url(base_url))
 
         self.assert_equals(lambda: self.selenium_instance.current_url, self.get_url('/initial-setup'))
