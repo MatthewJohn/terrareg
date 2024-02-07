1809f6e9f32cb88ac02377d8913ac7dc5ab4aead
Matthew John <matthew@dockstudios.co.uk>
2024-02-07 07:42:25 +0000

    feat: Add new base classes and factory for handling file storage
    Issue #491

---
+++ terrareg/file_storage.py
@@ -0,0 +1,87 @@
+
+
+from abc import ABC
+from io import TextIOWrapper
+import os
+import shutil
+
+import terrareg.config
+
+
+class BaseFileStorage(ABC):
+
+    def upload_file(self, source_path: str, dest: str):
+        ...
+
+    def file_exists(self, path: str) -> bool:
+        ...
+
+    def delete_file(self, path: str):
+        ...
+
+    def read_file(self, path: str, bytes_mode: bool=False) -> TextIOWrapper:
+        ...
+
+
+class LocalFileStorage(BaseFileStorage):
+
+    def __init__(self, base_directory):
+        """Store base directory"""
+        self._base_directory = base_directory
+
+    def _generate_path(self, *paths: str) -> str:
+        return os.path.join(self._base_directory, *paths)
+
+    def upload_file(self, source_path: str, dest_directory: str, dest_filename: str):
+        """Upload file"""
+        dest_directory = self._generate_path(dest_directory)
+        # Create all parent directories
+        os.makedirs(dest_directory, exist_ok=True)
+        dest_full_path = os.path.join(dest_directory, dest_filename)
+        # If destination already exists, but isnt a file, raise error.
+        if os.path.exists(dest_full_path) and not os.path.isfile(dest_full_path):
+            raise Exception("Destination already exists, but is not a file")
+
+        # Copy source file to destination
+        shutil.copyfile(source_path, dest_full_path)
+
+    def file_exists(self, path: str) -> bool:
+        """Return if a file exists"""
+        path = self._generate_path(path)
+        return os.path.isfile(path)
+
+    def delete_file(self, path: str):
+        """Delete path"""
+        path = self._generate_path(path)
+        if self.file_exists(path):
+            os.unlink(path)
+
+    def delete_directory(self, path: str):
+        """Delete path"""
+        path = self._generate_path(path)
+        raise Exception(f"Going to delete direcxtory: {path}")
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+    def read_file(self, path: str, bytes_mode: bool=False):
+        """Return filehandler for file"""
+        path = self._generate_path(path)
+        mode = "r"
+        if bytes_mode:
+            mode += "b"
+        return open(path, mode)
+
+
+class S3FileStorage(BaseFileStorage):
+    pass
+
+
+class FileStorageFactory:
+
+    def get_file_storage(self) -> 'BaseFileStorage':
+        """Generate file storage instance"""
+        config = terrareg.config.Config()
+        if config.DATA_DIRECTORY.startswith("s3://"):
+            return S3FileStorage(config.DATA_DIRECTORY)
+        else:
+            return LocalFileStorage(config.DATA_DIRECTORY)
