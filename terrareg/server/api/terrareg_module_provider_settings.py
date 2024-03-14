3f4c39a1a3fd98a10faa4426cc2fe6435737e0dd
Matthew John <matthew@dockstudios.co.uk>
2024-03-14 06:20:34 +0000

    chore: Move ModuleProviderUpdate POST API endpoint parser into arg parser method and update docs
    Issue #513

--- terrareg/server/api/terrareg_module_provider_settings.py
+++ terrareg/server/api/terrareg_module_provider_settings.py
@@ -20,8 +20,8 @@ class ApiTerraregModuleProviderSettings(ErrorCatchingResource):
         )
     ]
 
-    def _post(self, namespace, name, provider):
-        """Handle update to settings."""
+    def _post_arg_parser(self):
+        """Return arg parser for POST request"""
         parser = reqparse.RequestParser()
         parser.add_argument(
             'git_provider_id', type=str,
@@ -100,7 +100,11 @@ class ApiTerraregModuleProviderSettings(ErrorCatchingResource):
             location='json',
             default=None
         )
+        return parser
 
+    def _post(self, namespace, name, provider):
+        """Handle update to settings."""
+        parser = self._post_arg_parser()
         args = parser.parse_args()
 
         terrareg.csrf.check_csrf_token(args.csrf_token)
